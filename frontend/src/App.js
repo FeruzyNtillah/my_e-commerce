48f774882b41c56326263b2a9d83b3ff26dd4983
Karim Feruzy <167116240+FeruzyNtillah@users.noreply.github.com>
2026-02-15 17:06:05 +0300

    created an App.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,140 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import store from './redux/store';
+
+// Components
+import Header from './components/Header';
+import Footer from './components/Footer';
+import PrivateRoute from './components/PrivateRoute';
+
+// Pages
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
+import CartPage from './pages/CartPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ProfilePage from './pages/ProfilePage';
+import ShippingPage from './pages/ShippingPage';
+import PaymentPage from './pages/PaymentPage';
+import PlaceOrderPage from './pages/PlaceOrderPage';
+import OrderPage from './pages/OrderPage';
+
+// Admin Pages
+import ProductListPage from './pages/admin/ProductListPage';
+import ProductEditPage from './pages/admin/ProductEditPage';
+import OrderListPage from './pages/admin/OrderListPage';
+import UserListPage from './pages/admin/UserListPage';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <main className="main-content">
+            <Routes>
+              {/* Public Routes */}
+              <Route path="/" element={<HomePage />} />
+              <Route path="/product/:id" element={<ProductPage />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+
+              {/* Protected Routes */}
+              <Route
+                path="/shipping"
+                element={
+                  <PrivateRoute>
+                    <ShippingPage />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/payment"
+                element={
+                  <PrivateRoute>
+                    <PaymentPage />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/placeorder"
+                element={
+                  <PrivateRoute>
+                    <PlaceOrderPage />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/order/:id"
+                element={
+                  <PrivateRoute>
+                    <OrderPage />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/profile"
+                element={
+                  <PrivateRoute>
+                    <ProfilePage />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/orders"
+                element={
+                  <PrivateRoute>
+                    <ProfilePage />
+                  </PrivateRoute>
+                }
+              />
+
+              {/* Admin Routes */}
+              <Route
+                path="/admin/products"
+                element={
+                  <PrivateRoute adminOnly={true}>
+                    <ProductListPage />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/admin/product/:id/edit"
+                element={
+                  <PrivateRoute adminOnly={true}>
+                    <ProductEditPage />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/admin/orders"
+                element={
+                  <PrivateRoute adminOnly={true}>
+                    <OrderListPage />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/admin/users"
+                element={
+                  <PrivateRoute adminOnly={true}>
+                    <UserListPage />
+                  </PrivateRoute>
+                }
+              />
+            </Routes>
+          </main>
+          <Footer />
+          <ToastContainer position="top-right" autoClose={3000} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
